f571bb3f308ada2af2824538705dac8347cd8504
orszolka108 <orszolka108@gmail.com>
2018-08-01 07:59:38 +0200

    Stylesheet added

--- js/app.jsx
+++ js/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//require ("../scss/main.scss");
+require ("../scss/main.scss");
 
 //importing sentences database from js file
 import sentences from './sentences.js';
